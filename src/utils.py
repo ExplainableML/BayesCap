694904bf95d6e9e648dcc5fc4c9b3db86042217e
udion <uddeshya.upa@gmail.com>
2022-07-11 07:49:58 +0000

    separated gradio app

--- src/utils.py
+++ src/utils.py
@@ -22,6 +22,36 @@ from torchvision import transforms
 from torchvision.transforms import functional as F
 from losses import TempCombLoss
 
+######## for loading checkpoint from googledrive
+google_drive_paths = {
+    "BayesCap_SRGAN.pth": "https://drive.google.com/uc?id=1d_5j1f8-vN79htZTfRUqP1ddHZIYsNvL",
+    "BayesCap_ckpt.pth": "https://drive.google.com/uc?id=1Vg1r6gKgQ1J3M51n6BeKXYS8auT9NhA9",
+}
+
+def ensure_checkpoint_exists(model_weights_filename):
+    if not os.path.isfile(model_weights_filename) and (
+        model_weights_filename in google_drive_paths
+    ):
+        gdrive_url = google_drive_paths[model_weights_filename]
+        try:
+            from gdown import download as drive_download
+
+            drive_download(gdrive_url, model_weights_filename, quiet=False)
+        except ModuleNotFoundError:
+            print(
+                "gdown module not found.",
+                "pip3 install gdown or, manually download the checkpoint file:",
+                gdrive_url
+            )
+
+    if not os.path.isfile(model_weights_filename) and (
+        model_weights_filename not in google_drive_paths
+    ):
+        print(
+            model_weights_filename,
+            " not found, you may need to manually download the model weights."
+        )
+
 ########### DeblurGAN function
 def get_norm_layer(norm_type='instance'):
     if norm_type == 'batch':
